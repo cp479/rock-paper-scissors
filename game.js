dfc68f19d6549e3bb84fd78e26f630cafbcb8da4
cp479 <157428748+cp479@users.noreply.github.com>
2024-03-11 22:05:04 -0600

    Test computer selection

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
-function getComputerSelection() {
-    // Make the computer choose a number for 0 (rock), 1 (paper), 2 (scissors)
-        let randomNumber = Math.floor(Math.random()*3);
-        if (randomNumber == 0) {
-            return "rock";
-            }
-            else if (randomNumber == 1) {
-                return "paper";
-            }
-            else {
-                return "scissors";
-            }
-            // Use console.log(randomNumber); to confirm
-    }
+
+
+    function getComputerSelection() {
+        // Create a variable randomNumber between 0 - 2
+            let randomNumber = Math.floor(Math.random()*3);
+            // console.log(randomNumber);
+        // Set getComputerSelection to "rock" (0), "paper" (1) or "scissors"(2)
+            if (randomNumber == 0) {
+                return "rock";
+                }
+                else if (randomNumber == 1) {
+                    return "paper";
+                }
+                else {
+                    return "scissors";
+                }
+        }
