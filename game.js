2dbe350e95332bd92c46f42eb54969d1a0402e51
cp479 <157428748+cp479@users.noreply.github.com>
2024-03-11 22:00:56 -0600

    Add function getComputerSelection

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    return choices [Math.floor[Math.random()*items.length]];
-    console.log (getComputerChoice);
-}
+function getComputerSelection() {
+    // Make the computer choose a number for 0 (rock), 1 (paper), 2 (scissors)
+        let randomNumber = Math.floor(Math.random()*3);
+        if (randomNumber == 0) {
+            return "rock";
+            }
+            else if (randomNumber == 1) {
+                return "paper";
+            }
+            else {
+                return "scissors";
+            }
+            // Use console.log(randomNumber); to confirm
+    }
